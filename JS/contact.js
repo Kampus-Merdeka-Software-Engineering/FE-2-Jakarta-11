42d4bd40fbee406f8d583e89e8b03f82547564ab
HilmanZy <hilmanfatoni8@gmail.com>
2023-11-26 06:26:19 +0700

    new_update

--- JS/contact.js
+++ JS/contact.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    const showDataDiv = document.getElementById('user-data-display');
+    const form = document.getElementById('contactForm');
+    const showDataSection = document.querySelector('.show-data');
 
-    // Function to post data to server and display response
-    function postData(formDataObj) {
-        fetch('https://magenta-rose-raven-hat.cyclic.app/submit', { // Ganti dengan URL server Anda
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Constructing the form data object
+        const formDataObj = {
+            'First Name': document.querySelector('.fname').value,
+            'Last Name': document.querySelector('.lname').value,
+            'Contact Number': document.querySelector('#phoneNum').value,
+            Email: document.querySelector('#email').value,
+            Subject: document.querySelector('.subject').value,
+            'Company Name': document.querySelector('.company1').value,
+            Message: document.querySelector('textarea[name="message"]').value
+        };
+
+        // Sending data to the server
+        fetch('https://magenta-rose-raven-hat.cyclic.app/submit', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -13,20 +26,22 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(response => response.json())
         .then(data => {
-            showDataDiv.innerHTML = `
+            // Display the response data without the ID
+            const userData = data.data;
+            showDataSection.innerHTML = `
                 <div class="sign-box">
                     <h1>Send Successfully</h1>
-                    ${Object.entries(data.data).map(([key, value]) => `<p>${key}: ${value}</p>`).join('')}
+                    <h3>First Name: <p>${userData['First Name']}</p></h3>
+                    <h3>Last Name: <p>${userData['Last Name']}</p></h3>
+                    <h3>Contact Number: <p>${userData['Contact Number']}</p></h3>
+                    <h3>Email: <p>${userData.Email}</p></h3>
+                    <h3>Subject: <p>${userData.Subject}</p></h3>
+                    <h3>Company Name: <p>${userData['Company Name']}</p></h3>
+                    <h3>Your Message: <p>${userData.Message}</p></h3>
                 </div>
             `;
+            showDataSection.style.display = 'block';
         })
-        .catch(error => console.error('Error posting data:', error));
-    }
-
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        const formData = new FormData(form);
-        const formDataObj = Object.fromEntries(formData);
-        postData(formDataObj);
+        .catch(error => console.error('Error:', error));
     });
 });
