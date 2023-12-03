5127ffb24b4dc7065f0c8aa5bf8eae49e6a87699
HilmanZy <hilmanfatoni8@gmail.com>
2023-12-03 16:14:16 +0700

    sorehari

--- JS/contact.js
+++ JS/contact.js
@@ -11,34 +11,28 @@ document.getElementById('contactForm').addEventListener('submit', function(e){
     message: document.querySelector('textarea[name="message"]').value
   };
 
-  axios.post('https://magenta-rose-raven-hat.cyclic.app/contact/submit', data)
+  // Menggunakan Axios untuk mengirim data ke backend
+  axios.post('https://magenta-rose-raven-hat.cyclic.app/contact', data)
     .then(response => {
       console.log('Success:', response.data);
-
-      // Dapatkan elemen showData
-      const showData = document.getElementById('showData');
-      
-      // Tampilkan data pada showData
-      showData.innerHTML = `
-        <h1>Send Successfully</h1>
-        <h3>First Name: <p>${data.firstName}</p></h3>
-        <h3>Last Name: <p>${data.lastName}</p></h3>
-        <h3>Contact Number: <p>${data.phoneNumber}</p></h3>
-        <h3>Email: <p>${data.email}</p></h3>
-        <h3>Subject: <p>${data.subject}</p></h3>
-        <h3>Company Name: <p>${data.company}</p></h3>
-        <h3>Your Message: <p>${data.message}</p></h3>
-      `;
-
-      // Tampilkan showData
-      showData.style.display = 'block';
+      // Memperbarui UI untuk menampilkan data yang disubmit
+      updateUI(response.data);
     })
     .catch(error => {
       console.error('Error:', error);
-
-      // Jika ada kesalahan, tampilkan pesan gagal pada showData
-      const showData = document.getElementById('showData');
-      showData.innerHTML = '<h1>Failed to Send</h1>';
-      showData.style.display = 'block';
     });
 });
+
+function updateUI(data) {
+  // Memperbarui elemen-elemen di "SECTION DISPLAY FORM" sesuai data yang disubmit
+  document.getElementById('showData').innerHTML = `
+    <h1>Send Successfully</h1>
+    <h3>First Name: <p>${data.firstName}</p></h3>
+    <h3>Last Name: <p>${data.lastName}</p></h3>
+    <h3>Contact Number: <p>${data.phoneNumber}</p></h3>
+    <h3>Email: <p>${data.email}</p></h3>
+    <h3>Subject: <p>${data.subject}</p></h3>
+    <h3>Company Name: <p>${data.company}</p></h3>
+    <h3>Your Message: <p>${data.message}</p></h3>
+  `;
+}
