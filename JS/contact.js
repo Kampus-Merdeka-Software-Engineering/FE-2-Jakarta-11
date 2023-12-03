0bb973ab759510bd6e643d32b9ed4cd26af3c7bc
HilmanZy <hilmanfatoni8@gmail.com>
2023-12-03 14:07:25 +0700

    newesty

--- JS/contact.js
+++ JS/contact.js
@@ -2,53 +2,43 @@ document.getElementById('contactForm').addEventListener('submit', function(e){
   e.preventDefault();
 
   const data = {
-      firstName: document.getElementById('fname').value,
-      lastName: document.getElementById('lname').value,
-      phoneNumber: document.getElementById('phone').value,
-      email: document.getElementById('email').value,
-      subject: document.getElementById('subject').value,
-      company: document.getElementById('company').value,
-      message: document.querySelector('textarea[name="message"]').value
+    firstName: document.getElementById('fname').value,
+    lastName: document.getElementById('lname').value,
+    phoneNumber: document.getElementById('phone').value,
+    email: document.getElementById('email').value,
+    subject: document.getElementById('subject').value,
+    company: document.getElementById('company').value,
+    message: document.querySelector('textarea[name="message"]').value
   };
 
-  fetch('https://magenta-rose-raven-hat.cyclic.app/contact', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-  })
-  .then(response => {
-      if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-  })
-  .then(data => {
-      if (data.contact) {
-          updateDisplayForm(data.contact);
-          sessionStorage.setItem('contactData', JSON.stringify(data.contact));
-      }
-      console.log('Success:', data);
-  })
-  .catch((error) => {
+  axios.post('https://magenta-rose-raven-hat.cyclic.app/contact/submit', data)
+    .then(response => {
+      console.log('Success:', response.data);
+
+      // Dapatkan elemen showData
+      const showData = document.getElementById('showData');
+      
+      // Tampilkan data pada showData
+      showData.innerHTML = `
+        <h1>Send Successfully</h1>
+        <h3>First Name: <p>${data.firstName}</p></h3>
+        <h3>Last Name: <p>${data.lastName}</p></h3>
+        <h3>Contact Number: <p>${data.phoneNumber}</p></h3>
+        <h3>Email: <p>${data.email}</p></h3>
+        <h3>Subject: <p>${data.subject}</p></h3>
+        <h3>Company Name: <p>${data.company}</p></h3>
+        <h3>Your Message: <p>${data.message}</p></h3>
+      `;
+
+      // Tampilkan showData
+      showData.style.display = 'block';
+    })
+    .catch(error => {
       console.error('Error:', error);
-  });
+
+      // Jika ada kesalahan, tampilkan pesan gagal pada showData
+      const showData = document.getElementById('showData');
+      showData.innerHTML = '<h1>Failed to Send</h1>';
+      showData.style.display = 'block';
+    });
 });
-
-function updateDisplayForm(contact) {
-  document.getElementById('displayFirstName').textContent = contact.firstName;
-  document.getElementById('displayLastName').textContent = contact.lastName;
-  document.getElementById('displayPhoneNumber').textContent = contact.phoneNumber;
-  document.getElementById('displayEmail').textContent = contact.email;
-  document.getElementById('displaySubject').textContent = contact.subject;
-  document.getElementById('displayCompany').textContent = contact.company;
-  document.getElementById('displayMessage').textContent = contact.message;
-}
-
-window.onload = () => {
-  const savedData = JSON.parse(sessionStorage.getItem('contactData'));
-  if (savedData) {
-      updateDisplayForm(savedData);
-  }
-};
